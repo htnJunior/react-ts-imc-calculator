c7e7c618f25dbeb4c8ae0d62da8be0b155b67df4
hso <hso@certi.org.br>
2022-03-23 15:26:57 -0300

    Finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredBy from './assets/powered.png';
-import { levels, calculateImc} from './helpers/imc';
+import leftArrowImage from './assets/leftarrow.png'
+import { levels, calculateImc, Level} from './helpers/imc';
 import { GridItem } from './components/GridItem'
 
 const App = () => {
   const [heightField, setHeightField] = useState<number>(0);
   const [weightField, setWeightField] = useState<number>(0);
+  const [showItem, setShowItem] = useState<Level | null>(null);
 
   const handleCalculateImc = () => {
     if(heightField && weightField){
-      
+      setShowItem(calculateImc(heightField, weightField))
     }else{
       alert("Digite todos os campos")
     }
+  }
+
+  const handleBackButton = () => {
+    setShowItem(null)
+    setHeightField(0)
+    setWeightField(0)
   }
 
 
@@ -30,10 +38,11 @@ const App = () => {
           <p>O índice de massa corporal (IMC) é uma medida internacional usada para calcular se uma pessoa está no peso ideal.</p>
 
           <input 
-          type="number" 
+          type="number"
           placeholder="Digite a sua altura. Ex: 1.5(em metros)"
           value={heightField > 0 ? heightField : ''}
           onChange={e => setHeightField(parseFloat(e.target.value))}
+          disabled={showItem ? true : false}
           />
 
           <input 
@@ -41,16 +50,27 @@ const App = () => {
           placeholder="Digite o seu peso. Ex: 80.5(em kg)"
           value={weightField > 0 ? weightField : ''}
           onChange={e => setWeightField(parseFloat(e.target.value))}
+          disabled={showItem ? true : false}
           />
 
-          <button onClick={handleCalculateImc}>Calcular IMC</button>
+          <button onClick={handleCalculateImc} disabled={showItem ? true : false}>Calcular IMC</button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, key) => (
-              <GridItem key={key} item={item} />
-            ))}
-          </div>
+          {!showItem &&
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          }
+          {showItem &&
+            <div className={styles.rightToBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={showItem} />
+            </div>
+          }
         </div>
       </div>
     </div>
--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -8,7 +8,7 @@ export type Level = {
 
 export const levels: Level[] = [
     {title: 'Baixo peso', color: '#96A3AB', icon: 'down', imc: [0, 18.5]},
-    {title: 'Peso normal', color: '#EAD69', icon: 'up', imc: [18.6, 24.9]},
+    {title: 'Peso normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
     {title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30]},
     {title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99]}
 ]
@@ -18,8 +18,9 @@ export const calculateImc = (height:number, weight:number) => {
 
     for(let i in levels){
         if(imc >= levels[i].imc[0] && imc < levels[i].imc[1]){
-            levels[i].yourImc = imc
-            return levels[i]
+            let levelCopy: Level = {...levels[i]}
+            levelCopy.yourImc = parseFloat(imc.toFixed(2))
+            return levelCopy
         }
     }
 
